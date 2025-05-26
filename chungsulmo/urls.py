3cd949e02bbdb562950e41093198b7efa43e1f59
PAUL <kimjs20020426@gmail.com>
2025-05-26 16:02:16 +0900

    회원가입 및 로그인 기능 구현

--- chungsulmo/urls.py
+++ chungsulmo/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('community.urls')),  # community 앱으로 연결,
+    path('', include('accounts.urls')),
+    path('', lambda request: redirect('community:index')),
 ]
--- community/urls.py
+++ community/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
+
+app_name = 'community'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('accounts/', include('accounts.urls'))
 ]
