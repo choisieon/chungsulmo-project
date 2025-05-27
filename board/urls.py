7f9a1fd87a532d26fccf6e9a090c1e622b8a9913
PAUL <kimjs20020426@gmail.com>
2025-05-27 14:27:48 +0900

    로그인 및 회원가입 기능 구체화

--- board/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.post_list, name='post_list'),           # 글 목록
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),  # 글 상세
-    path('post/new/', views.post_new, name='post_new'),    # 글쓰기
-    path('post/<int:pk>/like/', views.post_like, name='post_like'),
-]
--- community/urls.py
+++ community/urls.py
@@ -6,5 +6,9 @@ app_name = 'community'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+
+    path('conservation/', views.conservation, name='conservation'),
+    path('mentoring/', views.mentoring, name='mentoring'),
+    path('mypage/', views.mypage, name='mypage'),
 ]
